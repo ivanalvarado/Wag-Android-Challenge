ac54d35e013db1e113b66907a4ddf9d9824f281e
Ivan Alvarado <ivalvara21@gmail.com>
2018-02-28 10:59:22 -0800

    - converted ListView layout into SwipeRefreshLayout - added progress bar whenever data is being fetched from the server

--- Wag_Android_Challenge/app/src/main/java/com/ivanalvaradoapps/wagandroidchallenge/MainActivity.java
+++ Wag_Android_Challenge/app/src/main/java/com/ivanalvaradoapps/wagandroidchallenge/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ivanalvaradoapps.wagandroidchallenge;
 
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.ivanalvaradoapps.wagandroidchallenge.adapters.StackOverflowUserListAdapter;
 import com.ivanalvaradoapps.wagandroidchallenge.model.SOUsers;
@@ -25,23 +28,57 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
+    private final boolean PICASSO_DEBUG = true;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ProgressBar progressBar;
+    private TextView progressText;
+    private ListView usersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Sets Global Singleton Picasso Instance
-        Picasso.Builder builder = new Picasso.Builder(this);
-        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
-        Picasso built = builder.build();
-        built.setIndicatorsEnabled(true);
-        built.setLoggingEnabled(true);
-        Picasso.setSingletonInstance(built);
+        progressBar = findViewById(R.id.user_list_progress_bar);
+        progressText = findViewById(R.id.user_list_progress_text_view);
+        usersList = findViewById(R.id.so_user_list);
 
+        setSwipeGestureResponse();
+        buildPicassoSingletonInstance();
         fetchSoUsers();
     }
 
+    /**
+     * Sets a swipe listener to our SwipeRefreshLayout.
+     */
+    private void setSwipeGestureResponse() {
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                usersList.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                progressText.setVisibility(View.VISIBLE);
+                fetchSoUsers();
+            }
+        });
+    }
+
+    /**
+     * Sets Global Singleton Picasso Instance.
+     */
+    private void buildPicassoSingletonInstance() {
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(PICASSO_DEBUG);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+    }
+
+    /**
+     * Fetches the StackOverflow users list from the API endpoint using Retrofit.
+     */
     private void fetchSoUsers() {
         RestClient rc = new RestClient();
         Call<SOUsers> call = rc.getsStackOverflowService().getUsers();
@@ -49,14 +86,15 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<SOUsers>() {
             @Override
             public void onResponse(Call<SOUsers> call, final Response<SOUsers> response) {
-                if (response.isSuccessful()) { // Success
-                    Log.d(TAG, "Response Message: " + response.message());
-                    Log.d(TAG, "count = " + response.body().getItems().size());
+                progressBar.setVisibility(View.GONE);
+                progressText.setVisibility(View.GONE);
+                swipeRefreshLayout.setRefreshing(false);
 
+                if (response.isSuccessful()) { // Success
                     StackOverflowUserListAdapter listAdapter = new StackOverflowUserListAdapter(MainActivity.this, response.body().getItems());
-                    ListView userList = MainActivity.this.findViewById(R.id.so_user_list);
-                    userList.setAdapter(listAdapter);
-                    userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    usersList.setVisibility(View.VISIBLE);
+                    usersList.setAdapter(listAdapter);
+                    usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                             Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
@@ -67,12 +105,16 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Log.e(TAG, "Error: " + response.errorBody());
                 }
+
+
             }
 
             @Override
             public void onFailure(Call<SOUsers> call, Throwable t) {
                 Log.e(TAG, "Failed to connect to server: " + t.getMessage());
-
+                progressBar.setVisibility(View.GONE);
+                progressText.setVisibility(View.GONE);
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
